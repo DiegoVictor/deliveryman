ed12e1ade3442e997b6dfc0685c0c67f8afc1b36
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-12-23 20:45:39 -0300

    Create user creation use case

---
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -0,0 +1,33 @@
+import { hash } from "bcrypt";
+import { prisma } from "../../../../database/prisma";
+
+interface IClient {
+  username: string;
+  password: string;
+}
+
+export class CreateClientUseCase {
+  async execute({ username, password }: IClient) {
+    const client = await prisma.clients.findFirst({
+      where: {
+        username: {
+          mode: "insensitive",
+          equals: username,
+        },
+      },
+    });
+
+    if (client) {
+      throw new Error("CLient already exists!");
+    }
+
+    const paswordHash = await hash(password, 10);
+
+    return prisma.clients.create({
+      data: {
+        username,
+        password: paswordHash,
+      },
+    });
+  }
+}
