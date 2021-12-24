2226ec63a2deca3cd56a70ef4c97ffff91dc6742
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-12-24 13:15:21 -0300

    Configure routes and json parser

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import express from "express";
 
+import { routes } from "./routes";
+
 const app = express();
+
+app.use(express.json());
+app.use("/v1", routes);
 
 app.listen(process.env.PORT, () => {
   console.log("Deliveryman is on air!");
