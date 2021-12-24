29679cf251dc0fcbe1884f6b686d4173ffbdcb57
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-12-24 13:34:11 -0300

    Create client's authentication use case

---
+++ src/modules/accounts/authenticateUser/AuthenticateClientUseCase.ts
@@ -0,0 +1,33 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { prisma } from "../../../database/prisma";
+
+interface IClient {
+  username: string;
+  password: string;
+}
+
+export class AuthenticateClientUseCase {
+  async execute({ username, password }: IClient) {
+    const client = await prisma.clients.findFirst({
+      where: {
+        username,
+      },
+    });
+
+    if (!client) {
+      throw new Error("Username or password incorrect");
+    }
+
+    if (await compare(password, client.password)) {
+      const token = sign({ username }, String(process.env.JWT_SECRET), {
+        subject: client.id,
+        expiresIn: process.env.JWT_EXPIRATION,
+      });
+
+      return token;
+    }
+
+    throw new Error("Username or password incorrect");
+  }
+}
