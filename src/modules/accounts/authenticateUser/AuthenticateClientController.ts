cc68a1cc11a228cb94495e75f2fe82331a9896ff
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-12-24 13:34:28 -0300

    Create client's authentication controller

---
+++ src/modules/accounts/authenticateUser/AuthenticateClientController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+import { AuthenticateClientUseCase } from "./AuthenticateClientUseCase";
+
+export class AuthenticateClientController {
+  async handle(request: Request, response: Response) {
+    const { username, password } = request.body;
+
+    const authenticateClientUseCase = new AuthenticateClientUseCase();
+    const token = await authenticateClientUseCase.execute({
+      username,
+      password,
+    });
+
+    return response.json({ token });
+  }
+}
