da47abf7b0d025edbf19fd72bb1244243e84d454
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-12-26 10:28:05 -0300

    Fix variable case

--- src/modules/clients/useCases/createClient/CreateClientController.ts
+++ src/modules/clients/useCases/createClient/CreateClientController.ts
@@ -6,8 +6,8 @@ export class CreateClientController {
   async handle(request: Request, response: Response) {
     const { username, password } = request.body;
 
-    const createCLientUseCase = new CreateClientUseCase();
-    const result = await createCLientUseCase.execute({ username, password });
+    const createClientUseCase = new CreateClientUseCase();
+    const result = await createClientUseCase.execute({ username, password });
 
     return response.status(201).json(result);
   }
