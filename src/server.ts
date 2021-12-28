05d761203a44d092909b44cd77078c5566e8ea2c
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-12-28 10:32:09 -0300

    Create error handler

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import 'express-async-errors';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
 import { routes } from './routes';
 
@@ -7,6 +7,18 @@ const app = express();
 
 app.use(express.json());
 app.use('/v1', routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof Error) {
+    return response.status(400).json({
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    message: 'Internal Server Error',
+  });
+});
 
 app.listen(process.env.PORT, () => {
   console.log('Deliveryman is on air!');
