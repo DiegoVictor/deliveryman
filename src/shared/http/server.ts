e0abfa827e2974c4efdf8b57a4a4b33070c544b4
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-03 10:33:55 -0300

    Move to shared folder

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,13 +3,14 @@ import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 
-import { routes } from './routes';
+import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+
 app.use('/v1', routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
