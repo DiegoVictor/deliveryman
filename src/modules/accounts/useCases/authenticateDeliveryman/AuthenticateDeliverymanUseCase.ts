f894ac9090b4fab1424acd407101755143565d91
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-05 19:22:28 -0300

    Create deliveryman authentication use case

---
+++ src/modules/accounts/useCases/authenticateDeliveryman/AuthenticateDeliverymanUseCase.ts
@@ -0,0 +1,34 @@
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+
+import { prisma } from '../../../../database/prisma';
+import { IAccount } from '../../contracts/IAccount';
+
+export class AuthenticateDeliverymanUseCase {
+  async execute({ username, password }: IAccount) {
+    const deliveryman = await prisma.deliveryman.findFirst({
+      where: {
+        username,
+      },
+    });
+
+    if (!deliveryman) {
+      throw new Error('Username or password incorrect');
+    }
+
+    if (await compare(password, deliveryman.password)) {
+      const token = sign(
+        { username },
+        String(process.env.JWT_DELIVERYMAN_SECRET),
+        {
+          subject: deliveryman.id,
+          expiresIn: process.env.JWT_EXPIRATION,
+        }
+      );
+
+      return token;
+    }
+
+    throw new Error('Username or password incorrect');
+  }
+}
