945357f10d44a344e9d4e913271837754ad12655
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-05 19:21:18 -0300

    Create deliveryman creation endpoint

--- src/shared/http/routes/deliveryman.ts
+++ src/shared/http/routes/deliveryman.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
 
+import { CreateDeliverymanController } from '../../../modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
+import { usernameAndPasswordValidator } from '../validators/usernameAndPassword';
 const app = Router();
 
+const createDeliverymanController = new CreateDeliverymanController();
+app.post('/', usernameAndPasswordValidator, createDeliverymanController.handle);
 export default app;
