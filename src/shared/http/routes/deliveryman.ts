e8ea14d72b5f6b2a127fa0a2eb3286e3d27fe241
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-05 19:23:37 -0300

    Create authentication endpoint

--- src/shared/http/routes/deliveryman.ts
+++ src/shared/http/routes/deliveryman.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express';
 
+import { AuthenticateDeliverymanController } from '../../../modules/accounts/useCases/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { CreateDeliverymanController } from '../../../modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 import { usernameAndPasswordValidator } from '../validators/usernameAndPassword';
+
 const app = Router();
 
 const createDeliverymanController = new CreateDeliverymanController();
+const authenticateDeliverymanController =
+  new AuthenticateDeliverymanController();
+
 app.post('/', usernameAndPasswordValidator, createDeliverymanController.handle);
+app.post(
+  '/auth',
+  usernameAndPasswordValidator,
+  authenticateDeliverymanController.handle
+);
+
 export default app;
