1652824f7c501525807779a348cd5ea0d5c026bb
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-05 19:20:07 -0300

    Create deliveryman routes

---
+++ src/shared/http/routes/deliveryman.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const app = Router();
+
+export default app;
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 
 import clients from './clients';
+import deliveryman from './deliveryman';
 const app = Router();
+
 app.use('/clients', clients);
+app.use('/deliveryman', deliveryman);
 export default app;
