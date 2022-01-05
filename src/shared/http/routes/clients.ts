662177042e67b263e64dc45dc508459a8cfe683e
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-05 19:18:25 -0300

    Set validators

--- src/shared/http/routes/clients.ts
+++ src/shared/http/routes/clients.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 
+import { usernameAndPasswordValidator } from '../validators/usernameAndPassword';
 const app = Router();
 
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
 
-app.post('/', createClientController.handle);
-app.post('/auth', authenticateClientController.handle);
+app.post('/', usernameAndPasswordValidator, createClientController.handle);
+app.post(
+  '/auth',
+  usernameAndPasswordValidator,
+  authenticateClientController.handle
+);
+
+
 export default app;
