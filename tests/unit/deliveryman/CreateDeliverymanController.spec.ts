03d08ac6e35b0f3f9890ff65f3d6218b0e559356
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-06 20:20:51 -0300

    Test deliveryman creation controller tests

---
+++ tests/unit/deliveryman/CreateDeliverymanController.spec.ts
@@ -0,0 +1,50 @@
+import { getMockReq, getMockRes } from '@jest-mock/express';
+import { hash } from 'bcrypt';
+import { randomUUID } from 'crypto';
+
+import factory from '../../utils/factory';
+import { CreateDeliverymanController } from '../../../src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
+import { IAccount } from '../../../src/modules/accounts/models/IAccount';
+
+const execute = jest.fn(async (_: IAccount) => ({}));
+jest.mock(
+  '../../../src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase',
+  () => {
+    return {
+      CreateDeliverymanUseCase: function UseCase() {
+        return {
+          execute: async (client: IAccount) => execute(client),
+        };
+      },
+    };
+  }
+);
+
+describe('CreateDeliverymanController', () => {
+  it('should be able to create a new client', async () => {
+    const client = await factory.attrs<IAccount>('Account');
+    const request = getMockReq({ body: client });
+
+    const { res: response } = getMockRes();
+    const createDeliverymanController = new CreateDeliverymanController();
+
+    const id = randomUUID();
+    const passwordHash = await hash(client.password, 10);
+    execute.mockReturnValue(
+      Promise.resolve({
+        ...client,
+        id,
+        password: passwordHash,
+      })
+    );
+
+    await createDeliverymanController.handle(request, response);
+
+    expect(response.status).toHaveBeenCalledWith(201);
+    expect(response.json).toHaveBeenCalledWith({
+      id,
+      username: client.username,
+      password: passwordHash,
+    });
+  });
+});
