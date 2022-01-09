c5bf93d9267649d4aedc863395767c03e998908e
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-09 11:41:48 -0300

    Test controller that finds client deliveries

---
+++ tests/unit/clients/FindClientDeliveriesController.spec.ts
@@ -0,0 +1,51 @@
+import { getMockReq, getMockRes } from '@jest-mock/express';
+import { randomUUID } from 'crypto';
+
+import factory from '../../utils/factory';
+import { IDelivery } from '../../../src/modules/deliveries/contracts/IDelivery';
+import { IAccount } from '../../../src/modules/accounts/contracts/IAccount';
+import { FindClientDeliveriesController } from '../../../src/modules/clients/useCases/findClientDeliveries/FindClientDeliveriesController';
+
+const execute = jest.fn(async (_: string) => ({}));
+jest.mock(
+  '../../../src/modules/clients/useCases/findClientDeliveries/FindClientDeliveriesUseCase',
+  () => {
+    return {
+      FindClientDeliveriesUseCase: function UseCase() {
+        return {
+          execute: async (client_id: string) => execute(client_id),
+        };
+      },
+    };
+  }
+);
+
+describe('FindClientDeliveriesController', () => {
+  it("should be able to find clients' deliveries", async () => {
+    const client_id = randomUUID();
+    const client = await factory.attrs<IAccount>('Account', { id: client_id });
+    const deliveries = await factory.attrsMany<IDelivery>('Delivery', 5, {
+      id: randomUUID,
+      client_id,
+    });
+    const request = getMockReq({ client_id });
+
+    const { res: response } = getMockRes();
+    const findClientDeliveriesController = new FindClientDeliveriesController();
+    execute.mockReturnValue(
+      Promise.resolve({
+        id: client_id,
+        username: client.username,
+        deliveries,
+      })
+    );
+
+    await findClientDeliveriesController.handle(request, response);
+
+    expect(response.json).toHaveBeenCalledWith({
+      id: client_id,
+      username: client.username,
+      deliveries,
+    });
+  });
+});
