32bc8e50119be3c8c322bd94d7af277a7da9d6a4
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-10 18:17:32 -0300

    Create IDelivery contracts

---
+++ src/modules/deliveries/contracts/IDelivery.ts
@@ -0,0 +1,8 @@
+export interface IDelivery {
+  id?: string;
+  client_id: string;
+  deliveryman_id: string | null;
+  product_name: string;
+  delivered_at: string | Date | null;
+  created_at: Date;
+}
