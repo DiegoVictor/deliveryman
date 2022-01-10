af7a9209f886becb4ec4f8732d1ffbbc0b13e819
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-10 18:19:37 -0300

    Create delivery factory

--- tests/utils/factory.ts
+++ tests/utils/factory.ts
@@ -1,6 +1,7 @@
 import faker from 'faker';
 import factory from 'factory-girl';
 
+import { IDelivery } from '../../src/modules/deliveries/contracts/IDelivery';
 import { IAccount } from '../../src/modules/accounts/contracts/IAccount';
 
 factory.define<IAccount>(
@@ -9,6 +10,18 @@ factory.define<IAccount>(
   {
     username: faker.internet.userName,
     password: faker.internet.password,
+  }
+);
+
+factory.define<IDelivery>(
+  'Delivery',
+  {},
+  {
+    client_id: faker.datatype.uuid,
+    deliveryman_id: null,
+    product_name: faker.commerce.productName,
+    delivered_at: null,
+    created_at: () => new Date(),
   }
 );
 
