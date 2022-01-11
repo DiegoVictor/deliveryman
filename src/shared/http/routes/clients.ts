6e4ce3b0134f2fb1ebdb940d3f584b78a076b2e1
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-11 19:26:04 -0300

    Configure delivery creation endpoint

--- src/shared/http/routes/clients.ts
+++ src/shared/http/routes/clients.ts
@@ -2,10 +2,14 @@ import { Router } from 'express';
 
 import { AuthenticateClientController } from '../../../modules/accounts/useCases/authenticateClient/AuthenticateClientController';
 import { CreateClientController } from '../../../modules/clients/useCases/createClient/CreateClientController';
+import { CreateDeliveryController } from '../../../modules/deliveries/useCases/createDelivery/CreateDeliveryController';
 import { usernameAndPasswordValidator } from '../validators/usernameAndPassword';
 import { ensureClientAuthentication } from '../middlewares/ensureClientAuthentication';
+import { productNameValidator } from '../validators/productName';
+
 const app = Router();
 
+const createDeliveryController = new CreateDeliveryController();
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
 const findClientDeliveriesController = new FindClientDeliveriesController();
@@ -20,4 +24,6 @@ app.post(
 app.use(ensureClientAuthentication);
 
 app.get('/deliveries', findClientDeliveriesController.handle);
+app.post('/deliveries', productNameValidator, createDeliveryController.handle);
+
 export default app;
