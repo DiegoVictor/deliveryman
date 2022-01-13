8531b789f0dfcfbe04cbb8362410d2ae4ba6c333
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-13 19:05:38 -0300

    Create route to get deliveries from deliveryman

--- src/shared/http/routes/deliveryman.ts
+++ src/shared/http/routes/deliveryman.ts
@@ -4,12 +4,15 @@ import { AuthenticateDeliverymanController } from '../../../modules/accounts/use
 import { CreateDeliverymanController } from '../../../modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 import { usernameAndPasswordValidator } from '../validators/usernameAndPassword';
 import { ensureDeliverymanAuthentication } from '../middlewares/ensureDeliverymanAuthentication';
+import { FindDeliverymanDeliveriesController } from '../../../modules/deliveryman/useCases/findDeliverymanDeliveries/FindDeliverymanDeliveriesController';
 
 const app = Router();
 
 const createDeliverymanController = new CreateDeliverymanController();
 const authenticateDeliverymanController =
   new AuthenticateDeliverymanController();
+const findDeliverymanDeliveriesController =
+  new FindDeliverymanDeliveriesController();
 
 app.post('/', usernameAndPasswordValidator, createDeliverymanController.handle);
 app.post(
@@ -19,4 +22,7 @@ app.post(
 );
 
 app.use(ensureDeliverymanAuthentication);
+
+app.get('/deliveries', findDeliverymanDeliveriesController.handle);
+
 export default app;
