4869a0398434d4b15943e174feaebc7398cb7977
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-13 19:07:19 -0300

    Collect coverage from middlewares

--- jest.config.ts
+++ jest.config.ts
@@ -20,7 +20,10 @@ export default {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['src/modules/**/*.ts'],
+  collectCoverageFrom: [
+    'src/modules/**/*.ts',
+    'src/shared/http/middlewares/*.ts',
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'tests/coverage',
