d1e163fb41d9000056a74ce32c4ca90a815a685e
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-14 18:31:30 -0300

    Configure route

--- src/shared/http/routes/deliveries.ts
+++ src/shared/http/routes/deliveries.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 
+import { FindNotDeliveredController } from '../../../modules/deliveries/useCases/findNotDelivered/FindNotDeliveredController';
 import { ensureDeliverymanAuthentication } from '../middlewares/ensureDeliverymanAuthentication';
 
 const app = Router();
 
+const findNotDeliveredController = new FindNotDeliveredController();
 app.use(ensureDeliverymanAuthentication);
+
+app.get('/not_delivered', findNotDeliveredController.handle);
+
 export default app;
