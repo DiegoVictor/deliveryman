0695d7a2a3a2e2177e0be09a36507d25e7e1ba62
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-15 11:55:51 -0300

    Add deliveries router

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,8 +2,12 @@ import { Router } from 'express';
 
 import clients from './clients';
 import deliveryman from './deliveryman';
+import deliveries from './deliveries';
+
 const app = Router();
 
 app.use('/clients', clients);
 app.use('/deliveryman', deliveryman);
+app.use('/deliveries', deliveries);
+
 export default app;
