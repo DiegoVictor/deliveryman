4be5057c108f10a4f871e24fc09cabeae3098c3f
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-16 11:15:05 -0300

    Create route to set delivery as completed

--- src/shared/http/routes/deliveries.ts
+++ src/shared/http/routes/deliveries.ts
@@ -2,11 +2,16 @@ import { Router } from 'express';
 
 import { FindNotDeliveredController } from '../../../modules/deliveries/useCases/findNotDelivered/FindNotDeliveredController';
 import { SetDeliveryDeliverymanController } from '../../../modules/deliveries/useCases/setDeliveryDeliveryman/SetDeliveryDeliverymanController';
+import { ensureDeliverymanAuthentication } from '../../infra/http/middlewares/ensureDeliverymanAuthentication';
+import { SetAsDeliveredController } from '../../../modules/deliveries/useCases/setAsDelivered/SetAsDeliveredController';
+import { idValidator } from '../../infra/http/validators/id';
 
 const app = Router();
 
 const findNotDeliveredController = new FindNotDeliveredController();
 const setDeliveryDeliverymanController = new SetDeliveryDeliverymanController();
+const setAsDeliveredController = new SetAsDeliveredController();
+
 app.use(ensureDeliverymanAuthentication);
 
 app.get('/not_delivered', findNotDeliveredController.handle);
@@ -14,6 +19,11 @@ app.patch(
   '/:id/set_deliveryman',
   idValidator,
   setDeliveryDeliverymanController.handle
+);
+app.patch(
+  '/:id/set_as_delivered',
+  idValidator,
+  setAsDeliveredController.handle
 );
 
 export default app;
