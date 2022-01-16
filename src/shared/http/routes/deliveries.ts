d417abfc7eab5cea829cced6e97881fbf0cc12d4
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-16 11:13:32 -0300

    Configure route to set delivery's deliveryman

--- src/shared/http/routes/deliveries.ts
+++ src/shared/http/routes/deliveries.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express';
 
 import { FindNotDeliveredController } from '../../../modules/deliveries/useCases/findNotDelivered/FindNotDeliveredController';
-import { ensureDeliverymanAuthentication } from '../middlewares/ensureDeliverymanAuthentication';
+import { SetDeliveryDeliverymanController } from '../../../modules/deliveries/useCases/setDeliveryDeliveryman/SetDeliveryDeliverymanController';
 
 const app = Router();
 
 const findNotDeliveredController = new FindNotDeliveredController();
+const setDeliveryDeliverymanController = new SetDeliveryDeliverymanController();
 app.use(ensureDeliverymanAuthentication);
 
 app.get('/not_delivered', findNotDeliveredController.handle);
+app.patch(
+  '/:id/set_deliveryman',
+  idValidator,
+  setDeliveryDeliverymanController.handle
+);
 
 export default app;
