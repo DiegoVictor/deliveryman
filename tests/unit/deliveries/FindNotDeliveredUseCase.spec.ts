4abded7b2ddeeecdc54892740c5f16b9c2264c88
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-17 18:48:53 -0300

    Create use case to find not delivered items

---
+++ tests/unit/deliveries/FindNotDeliveredUseCase.spec.ts
@@ -0,0 +1,28 @@
+import { prisma } from '../../../src/shared/infra/prisma/client';
+import factory from '../../utils/factory';
+import { FindNotDeliveredUseCase } from '../../../src/modules/deliveries/useCases/findNotDelivered/findNotDeliveredUseCase';
+import { IDelivery } from '../../../src/modules/deliveries/contracts/IDelivery';
+import { IAccount } from '../../../src/modules/accounts/contracts/IAccount';
+
+describe('FindNotDeliveredUseCase', () => {
+  it('should be able to find not delivered items', async () => {
+    const client = await factory.attrs<IAccount>('Account');
+    const { id: client_id } = await prisma.clients.create({
+      data: client,
+    });
+
+    const deliveries = await factory.attrsMany<IDelivery>('Delivery', 5, {
+      client_id,
+    });
+    await prisma.deliveries.createMany({
+      data: deliveries,
+    });
+
+    const findNotDeliveredUseCase = new FindNotDeliveredUseCase();
+    const response = await findNotDeliveredUseCase.execute();
+
+    deliveries.forEach(delivery => {
+      expect(response).toContainEqual({ id: expect.any(String), ...delivery });
+    });
+  });
+});
