ee88b292b22c33a97d0fead12822dd41bd81b362
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-19 08:39:44 -0300

    Update accounts interface

--- src/modules/accounts/contracts/IAccount.ts
+++ src/modules/accounts/contracts/IAccount.ts
@@ -1,5 +1,8 @@
+import { IDelivery } from '../../deliveries/contracts/IDelivery';
+
 export interface IAccount {
   id?: string;
   username: string;
   password: string;
+  deliveries?: IDelivery[];
 }
