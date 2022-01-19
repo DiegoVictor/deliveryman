e802c75c2248eff6daa1aac2b55ab228ba4fd902
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-19 08:40:40 -0300

    Set fields as optional

--- src/modules/deliveries/contracts/IDelivery.ts
+++ src/modules/deliveries/contracts/IDelivery.ts
@@ -1,8 +1,8 @@
 export interface IDelivery {
   id?: string;
   client_id: string;
-  deliveryman_id: string | null;
+  deliveryman_id?: string | null;
   product_name: string;
-  delivered_at: string | Date | null;
+  delivered_at?: string | Date | null;
   created_at: Date;
 }
