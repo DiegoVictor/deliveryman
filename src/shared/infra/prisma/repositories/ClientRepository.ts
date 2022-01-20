05277df6ee7209f748115097dc496e5d4b3b47cf
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-20 08:27:10 -0300

    Create client repository

---
+++ src/shared/infra/prisma/repositories/ClientRepository.ts
@@ -0,0 +1,6 @@
+import { IClientRepository } from '../../../../modules/accounts/contracts/IClientRepository';
+import { IAccount } from '../../../../modules/accounts/contracts/IAccount';
+import { prisma } from '../client';
+
+export class ClientRepository implements IClientRepository {
+}
