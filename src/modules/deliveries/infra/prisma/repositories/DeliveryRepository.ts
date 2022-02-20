6cb0f359da5d5045b61eaacb6b8979f30de5b2a7
Diego Victor <diegovictorgonzaga@gmail.com>
2022-02-20 00:53:29 -0300

    Create update delivery method

--- src/modules/deliveries/infra/prisma/repositories/DeliveryRepository.ts
+++ src/modules/deliveries/infra/prisma/repositories/DeliveryRepository.ts
@@ -1,4 +1,14 @@
+import { prisma } from '../../../../../shared/infra/prisma/client';
+import { IDelivery } from '../../../contracts/IDelivery';
 import { IDeliveryRepository } from '../../../contracts/IDeliveryRepository';
 
 export class DeliveryRepository implements IDeliveryRepository {
+  async updateById(id: string, data: Partial<IDelivery>) {
+    return prisma.deliveries.update({
+      where: {
+        id,
+      },
+      data,
+    });
+  }
 }
