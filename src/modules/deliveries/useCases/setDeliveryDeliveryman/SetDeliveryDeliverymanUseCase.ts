06181d70122a44e05e683dd75dd86da24486a011
Diego Victor <diegovictorgonzaga@gmail.com>
2022-02-20 00:53:57 -0300

    Use repository to update delivery

--- src/modules/deliveries/useCases/setDeliveryDeliveryman/SetDeliveryDeliverymanUseCase.ts
+++ src/modules/deliveries/useCases/setDeliveryDeliveryman/SetDeliveryDeliverymanUseCase.ts
@@ -1,18 +1,19 @@
-import { prisma } from '../../../../src/database/prisma';
 import { IDelivery } from '../../contracts/IDelivery';
+import { IDeliveryRepository } from '../../contracts/IDeliveryRepository';
 
 export class SetDeliveryDeliverymanUseCase {
+  private repository: IDeliveryRepository;
+
+  constructor(repository: IDeliveryRepository) {
+    this.repository = repository;
+  }
+
   async execute({
     id,
     deliveryman_id,
-  }: Pick<IDelivery, 'id' | 'deliveryman_id'>) {
-    return prisma.deliveries.update({
-      where: {
-        id,
-      },
-      data: {
-        deliveryman_id,
-      },
+  }: Required<Pick<IDelivery, 'id' | 'deliveryman_id'>>) {
+    return this.repository.updateById(id, {
+      deliveryman_id,
     });
   }
 }
