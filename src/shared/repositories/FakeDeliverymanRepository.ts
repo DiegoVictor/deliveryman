556e27af83b1a1a863d26130327cd0198599e858
Diego Victor <diegovictorgonzaga@gmail.com>
2022-02-23 17:37:39 -0300

    Implement fake methods

--- src/shared/repositories/FakeDeliverymanRepository.ts
+++ src/shared/repositories/FakeDeliverymanRepository.ts
@@ -1,5 +1,45 @@
-import { IAccount } from '../../modules/accounts/contracts/IAccount';
+import { randomUUID } from 'crypto';
 
-export class FakeDeliverymanRepository {
+import { IAccount } from '../../modules/accounts/contracts/IAccount';
+import { IDeliverymanRepository } from '../../modules/accounts/contracts/IDeliverymanRepository';
+
+export class FakeDeliverymanRepository implements IDeliverymanRepository {
   private repository: IAccount[] = [];
+
+  async findById(
+    id: string
+  ): Promise<Pick<IAccount, 'id' | 'deliveries' | 'username'> | null> {
+    const deliveryman = this.repository.find(
+      deliveryman => deliveryman.id === id
+    );
+
+    if (deliveryman) {
+      return deliveryman;
+    }
+
+    return null;
+  }
+
+  async findByUsername(username: string): Promise<IAccount | null> {
+    const deliveryman = this.repository.find(
+      deliveryman => deliveryman.username === username
+    );
+
+    if (deliveryman) {
+      return deliveryman;
+    }
+
+    return null;
+  }
+
+  async create({ username, password }: IAccount): Promise<IAccount> {
+    const deliveryman = {
+      id: randomUUID(),
+      username,
+      password,
+    };
+    this.repository.push(deliveryman);
+
+    return deliveryman;
+  }
 }
