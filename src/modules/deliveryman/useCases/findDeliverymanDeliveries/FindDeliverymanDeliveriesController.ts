5fafe38a97c1786fb74786cb22716060e3d265e0
Diego Victor <diegovictorgonzaga@gmail.com>
2022-02-25 20:13:11 -0300

    Create repository and pass as parameter

--- src/modules/deliveryman/useCases/findDeliverymanDeliveries/FindDeliverymanDeliveriesController.ts
+++ src/modules/deliveryman/useCases/findDeliverymanDeliveries/FindDeliverymanDeliveriesController.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from 'express';
 
+import { DeliverymanRepository } from '../../../../shared/infra/prisma/repositories/DeliverymanRepository';
+
 import { FindDeliverymanDeliveriesUseCase } from './FindDeliverymanDeliveriesUseCase';
 
 export class FindDeliverymanDeliveriesController {
   async handle(request: Request, response: Response) {
     const { deliveryman_id } = request;
 
+    const deliverymanRepository = new DeliverymanRepository();
     const findDeliverymanDeliveriesUseCase =
-      new FindDeliverymanDeliveriesUseCase();
+      new FindDeliverymanDeliveriesUseCase(deliverymanRepository);
     const deliveries = await findDeliverymanDeliveriesUseCase.execute(
       deliveryman_id
     );
