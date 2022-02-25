9006236014f1f5fe7c8f6f7d511a6851c65c485e
Diego Victor <diegovictorgonzaga@gmail.com>
2022-02-25 20:12:54 -0300

    Use repository

--- src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController.ts
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from 'express';
 
+import { DeliverymanRepository } from '../../../../shared/infra/prisma/repositories/DeliverymanRepository';
 import { CreateDeliverymanUseCase } from './CreateDeliverymanUseCase';
 
 export class CreateDeliverymanController {
   async handle(request: Request, response: Response) {
     const { username, password } = request.body;
 
-    const createDeliverymanUseCase = new CreateDeliverymanUseCase();
+    const deliverymanRepository = new DeliverymanRepository();
+    const createDeliverymanUseCase = new CreateDeliverymanUseCase(
+      deliverymanRepository
+    );
     const result = await createDeliverymanUseCase.execute({
       username,
       password,
